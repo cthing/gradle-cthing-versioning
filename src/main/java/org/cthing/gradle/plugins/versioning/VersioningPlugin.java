ed7eee6e563f616f0e1b59b98a11aeb72ba607c9
Baron Roberts <baron@cthing.com>
2025-10-04 01:00:50 -0700

    Eliminate project references from within tasks.

--- src/main/java/org/cthing/gradle/plugins/versioning/VersioningPlugin.java
+++ src/main/java/org/cthing/gradle/plugins/versioning/VersioningPlugin.java
@@ -14,13 +14,17 @@ import java.util.List;
 import java.util.Set;
 import java.util.regex.Pattern;
 
+import javax.inject.Inject;
+
 import org.cthing.projectversion.ProjectVersion;
 import org.gradle.api.GradleException;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.gradle.api.artifacts.Configuration;
 import org.gradle.api.artifacts.Dependency;
+import org.gradle.api.file.ProjectLayout;
 import org.gradle.api.plugins.BasePlugin;
+import org.gradle.api.tasks.TaskContainer;
 import org.gradle.api.tasks.TaskExecutionException;
 
 
@@ -54,6 +58,13 @@ public class VersioningPlugin implements Plugin<Project> {
                                                                     "settings.gradle",
                                                                     "settings.gradle.kts",
                                                                     "gradle/libs.versions.toml");
+
+    private final ProjectLayout projectLayout;
+
+    @Inject
+    public VersioningPlugin(final ProjectLayout projectLayout) {
+        this.projectLayout = projectLayout;
+    }
 
     @Override
     public void apply(final Project project) {
@@ -112,31 +123,35 @@ public class VersioningPlugin implements Plugin<Project> {
     private void createVersionFileTask(final Project project) {
         // Only write the file if this is the root project and "clean" is not the only task.
         if (project.equals(project.getRootProject()) && isNotCleanOnly(project)) {
-            project.getTasks().register(VERSION_FILE_TASK_NAME, task -> {
-                final File buildDir = project.getLayout().getBuildDirectory().get().getAsFile();
+            final TaskContainer tasks = project.getTasks();
+            final Set<Project> projects = project.getAllprojects();
+            final Project rootProject = project.getRootProject();
+
+            tasks.register(VERSION_FILE_TASK_NAME, task -> {
+                final File buildDir = this.projectLayout.getBuildDirectory().get().getAsFile();
                 final File projectVersionFile = new File(buildDir, PROJECT_VERSION_FILENAME);
 
                 // The project version file is the output
                 task.getOutputs().file(projectVersionFile);
 
                 // Build related files are the inputs.
-                project.getAllprojects().forEach(proj -> task.getInputs().file(proj.getBuildFile()));
+                projects.forEach(proj -> task.getInputs().file(proj.getBuildFile()));
                 BUILD_RELATED_FILES.forEach(filename -> {
-                    final File file = project.file(filename);
+                    final File file = this.projectLayout.getProjectDirectory().file(filename).getAsFile();
                     if (file.exists()) {
                         task.getInputs().file(file);
                     }
                 });
 
                 // If "clean" is one of the tasks, generate the project version file after it has run
-                if (project.getTasks().findByName(BasePlugin.CLEAN_TASK_NAME) != null) {
+                if (tasks.findByName(BasePlugin.CLEAN_TASK_NAME) != null) {
                     task.mustRunAfter(BasePlugin.CLEAN_TASK_NAME);
                 }
 
                 task.doLast(t -> {
                     try {
                         Files.writeString(projectVersionFile.toPath(),
-                                          project.getRootProject().getVersion().toString(),
+                                          rootProject.getVersion().toString(),
                                           StandardCharsets.UTF_8);
                     } catch (final IOException ex) {
                         throw new TaskExecutionException(task, ex);
